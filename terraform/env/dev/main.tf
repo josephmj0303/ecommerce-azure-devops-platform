67dfc8a3f934c9a446b822ea89c22c58bafe202d
josephmj0303 <joseph.mj@aol.com>
2026-03-01 19:07:47 +0000

    modified postgres module main.tf & variables.tf

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -42,16 +42,11 @@ module "frontend_admin" {
 }
 
 module "postgres" {
-  source              	 = "../../modules/postgres"
-  name                   = var.name
-  resource_group_name    = var.resource_group_name
-  location               = var.location
-  administrator_login    = var.admin_user
-  administrator_password = var.admin_password
-  version                = "17"
-  sku_name               = "B_Standard_B1ms"
-  storage_mb             = 32768
-  
-  public_network_access_enabled = true
-}
+  source              = "../../modules/postgres"
+  name                = var.postgres_name
+  location            = var.location
+  resource_group_name = module.rg.name
 
+  admin_user          = var.postgres_admin_user
+  admin_password      = var.postgres_admin_password
+}
