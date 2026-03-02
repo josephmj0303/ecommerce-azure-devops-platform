7849c85bbc87f7b2a1cd45797a8e6c8a8a3a88dd
josephmj0303 <joseph.mj@aol.com>
2026-03-02 06:52:06 +0000

    tfstate modified

--- terraform/env/dev/backend.tf
+++ terraform/env/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-joedevopslab-network"
+    resource_group_name  = "rg-terraform-state"
     storage_account_name = "joedevopstfstate"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
